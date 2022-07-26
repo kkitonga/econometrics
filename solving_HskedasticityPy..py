660bfc3037dc82cd00f8ed25d0ef8e2929ec9d9e
kkitonga <104992744+kkitonga@users.noreply.github.com>
2022-07-25 21:37:45 -0700

    Update solving_HskedasticityPy..py

--- solving_HskedasticityPy..py
+++ solving_HskedasticityPy..py
@@ -12,9 +12,9 @@ import statsmodels.api as sm
 from statsmodels.compat import lzip
 
 
-#===========================LOADING DATASETS==================================#
+#===========================LOADING DATASET==================================#
 
-#loading data
+#loading CPS1985 data from R datasets
 CPS1985 = sm.datasets.get_rdataset("CPS1985",package="AER")
 
 #converting to dataframe
@@ -36,7 +36,7 @@ print(dict(lzip(bp_test_name, bptest)))
 #====================RESOLVING HETEROSKEDASTICITY=============================#
 
                        #1.Adjusting functional forms
-#1.logging depedent variable
+#i.logging depedent variable
 log_reg = sm.formula.ols('np.log(wage) ~ experience + education',data)
 log_reg = log_reg.fit()
 print(log_reg.summary())
@@ -46,7 +46,7 @@ bptest2 = np.round(sm.stats.diagnostic.het_breuschpagan(log_reg.resid,log_reg.mo
 print(dict(lzip(bp_test_name2, bptest2)))
 
 
-                 #2.Logging dependent and independent variable
+#ii.Logging dependent and independent variable
 log_reg2 = sm.formula.ols('np.log(wage) ~ np.log(education) + experience',data)
 log_reg2 = log_reg2.fit()
 print(log_reg2.summary())
@@ -56,18 +56,18 @@ bptest3 = np.round(sm.stats.diagnostic.het_breuschpagan(log_reg2.resid,log_reg2.
 print(dict(lzip(bp_test_name3, bptest3)))
 
 
-                         #3.GLS estimation
+                         #2.GLS estimation
 reg2 = sm.formula.gls('wage ~ experience + education',data)
 reg2 = reg2.fit()
 print(reg2.summary())
 
 
-                        #4.Robust standard errors
+                        #3.Robust standard errors
 reg3 =sm.formula.ols('wage ~ experience + education', data)
 reg3 = reg3.fit(cov_type='HC1')
 print(reg3.summary())
 
-#============================================================================#
+#====================================END================================================#
 
 
 
